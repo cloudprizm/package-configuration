0d343c9edc6b4524b56ef7252303caf57ef624e6
Damian Baar <damian.baar@gmail.com>
2018-10-26 17:02:58 +0200

    adding typedoc file to be able latter on read correct version of branch - subrepo / repo

---
+++ typedoc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // read from .gitrepo or .git
+  'sourcefile-url-prefix': 'https://github.com/hungry-consulting/package-configuration/tree/master/src/',
+  mode: 'file',
+  target: 'ES6',
+  theme: 'minimal'
+}
